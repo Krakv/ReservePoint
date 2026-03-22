96556f722100e6af29671ea0f23bc71d3176b489
Igor Tokmakov <104667117+Krakv@users.noreply.github.com>
2026-03-22 12:17:16 +0500

    migration...

---
+++ ReservePoint.Infrastructure/Migrations/20260322071706_ChangeDB.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReservePoint.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Bookings");
+
+            migrationBuilder.AddColumn<string>(
+                name: "IdentityId",
+                table: "Bookings",
+                type: "character varying(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IdentityId",
+                table: "Bookings");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "Bookings",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+    }
+}
