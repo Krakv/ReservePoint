f01f075825775381bc58add607442fe0f483819c
Igor Tokmakov <104667117+Krakv@users.noreply.github.com>
2026-03-25 18:25:45 +0500

    Update OrgPolicyDto.cs

--- ReservePoint.Application/DTOs/OrgPolicyDto.cs
+++ ReservePoint.Application/DTOs/OrgPolicyDto.cs
@@ -1,10 +1,11 @@
-using System.Text.Json.Serialization;
-
 namespace ReservePoint.Application.DTOs;
 
 public record OrgPolicyDto(
-    [property: JsonPropertyName("organization_id")] Guid OrganizationId,
-    [property: JsonPropertyName("max_booking_duration_min")] int MaxBookingDurationMin,
-    [property: JsonPropertyName("booking_window_days")] int BookingWindowDays,
-    [property: JsonPropertyName("max_active_bookings_per_user")] int MaxActiveBookingsPerUser
+     int ID,
+     Guid OrganizationID,
+     int MaxBookingDurationMin,
+     int BookingWindowDays,
+     int MaxActiveBookingsPerUser,
+     DateTime CreatedAt,
+     DateTime UpdatedAt
 );
